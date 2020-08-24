f92da73b404d570b40fe2d25ead3d9b327944e7c
admin <admin@localhost.localdomain>
2020-08-24 23:27:16 +0800

    new add java's IO

---
+++ java/IOStreamLearn/src/FileTest.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+public class FileTest {
+    public static void main(String[] args) {
+        File file = new File("word.txt");
+        if(file.exists()){
+            String name = file.getName();
+            long length = file.length();
+            boolean hidden = file.isHidden();
+            System.out.println("文件名称：" + name);
+            System.out.println("文件长度：" + length);
+            System.out.println("该文件是隐藏文件吗？" + hidden);
+        }else {
+            System.out.println("文件不存在");
+        }
+    }
+}
